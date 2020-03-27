96151c48034706690884942ef67f52f0ed91dbfb
sandeep-chahal <sandeepchahal573@gmail.com>
2020-03-27 12:09:50 +0530

    add projects

--- src/Component/Projects/Project.js
+++ src/Component/Projects/Project.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import GithubPng from "../../Assets/github.png";
 import LinkPng from "../../Assets/link.svg";
+import VideoPng from "../../Assets/video.png";
 
 const Project = props => {
 	const [hover, setHover] = useState(false);
@@ -10,11 +11,18 @@ const Project = props => {
 			onMouseEnter={() => setHover(true)}
 			onMouseLeave={() => setHover(false)}
 		>
-			<img alt={props.title} src={hover ? props.gif : props.img} />
+			<div
+				className="img"
+				style={{
+					backgroundImage: `url(${hover ? props.gif : props.img})`
+				}}
+			>
+				{hover ? <img src={VideoPng} alt="playing video" /> : null}
+			</div>
 			<div className="title">{props.title}</div>
 			<div className="desc">{props.desc}</div>
-			<a href={props.github} rel="noopener noreferrer" target="_blank">
-				<img alt="demo" src={LinkPng} /> <spam>Demo</spam>
+			<a href={props.demo} rel="noopener noreferrer" target="_blank">
+				<img alt="demo" src={LinkPng} /> <span>Demo</span>
 			</a>
 			<a href={props.github} rel="noopener noreferrer" target="_blank">
 				<img alt="github" src={GithubPng} /> <span>Github</span>
--- src/Component/Projects/Projects.js
+++ src/Component/Projects/Projects.js
@@ -2,9 +2,53 @@ import React from "react";
 import "./Projects.scss";
 
 import Project from "./Project";
+import BudgetGif from "../../Assets/budget-gif.gif";
+import BudgetPng from "../../Assets/budget-img.png";
+import DiscordGif from "../../Assets/discord-gif.gif";
+import DiscordPng from "../../Assets/discord-img.png";
+import VocabGif from "../../Assets/vocab-gif.gif";
+import VocabPng from "../../Assets/vocab-img.png";
+import CoronaPng from "../../Assets/corona-img.png";
+import CoronaGif from "../../Assets/corona-gif.gif";
 
 const Projects = props => {
-	const projects = [];
+	const projects = [
+		{
+			title: "Discord Clone",
+			desc:
+				"A clone  app of popular chat website discord using react, redux and firebase.",
+			img: DiscordPng,
+			gif: DiscordGif,
+			github: "https://github.com/sandeep-chahal/discord-clone",
+			demo: "https://discordclone.netlify.com/"
+		},
+		{
+			title: "Corona Virus Dashboard",
+			desc: "Corona Virus webiste to check updates with map, charts and more. ",
+			img: CoronaPng,
+			gif: CoronaGif,
+			github: "https://github.com/sandeep-chahal/corona-virus-live",
+			demo: "https://covid-live.netlify.com/"
+		},
+		{
+			title: "Vocab Builder",
+			desc:
+				"A website to use subtitles of tv shows, movies and youtube to learn new vocabulary words.",
+			img: VocabPng,
+			gif: VocabGif,
+			github:
+				"https://github.com/sandeep-chahal/vocab-builder-with-movies-and-tvshows",
+			demo: "https://vocab-builder.netlify.com/"
+		},
+		{
+			title: "Budget Calculator",
+			desc: "A simple budget calculator app with react and firebase",
+			img: BudgetPng,
+			gif: BudgetGif,
+			github: "https://github.com/sandeep-chahal/budget-calculator",
+			demo: "https://budgtcalc.netlify.com/"
+		}
+	];
 
 	return (
 		<div className="projects" id="projects">
