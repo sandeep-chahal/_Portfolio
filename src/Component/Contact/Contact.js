9e8cbaae8c8e795eb69b39f784783fa9b1b5feb4
sandeep-chahal <sandeepchahal573@gmail.com>
2020-03-28 08:20:17 +0530

    send mail without redirecting

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -1,21 +1,54 @@
 import React from "react";
 import "./Contact.scss";
 
-const Contact = props => {
-	return (
-		<div className="contact" id="contact">
-			<h2>Contact</h2>
-			<form action="https://formspree.io/xyypaovk" method="POST" name="contact">
-				<input type="text" placeholder="Name" name="name" />
-				<input type="text" placeholder="Email" name="email" />
-				<textarea placeholder="message" name="msg" />
-				<button type="submit" value="send">
-					Shoot!
-				</button>
-			</form>
-			<div className="social"></div>
-		</div>
-	);
-};
+export default class Contact extends React.Component {
+	constructor(props) {
+		super(props);
+		this.submitForm = this.submitForm.bind(this);
+		this.state = {
+			status: ""
+		};
+	}
 
-export default Contact;
+	render() {
+		const { status } = this.state;
+		return (
+			<div className="contact" id="contact">
+				<h2>Contact</h2>
+				<form
+					onSubmit={this.submitForm}
+					action="https://formspree.io/xyypaovk"
+					method="POST"
+				>
+					<input type="text" placeholder="Name" name="name" />
+					<input type="text" placeholder="Email" name="email" />
+					<textarea placeholder="message" name="msg" />
+					{status === "SUCCESS" ? <div>Thanks!</div> : <button>Shoot!</button>}
+					{status === "ERROR" && <div>Ooops! There was an error.</div>}
+					{status === "SENDING" && <div>Sending!</div>}
+				</form>
+				<div className="social"></div>
+			</div>
+		);
+	}
+
+	submitForm(ev) {
+		ev.preventDefault();
+		this.setState({ status: "SENDING" });
+		const form = ev.target;
+		const data = new FormData(form);
+		const xhr = new XMLHttpRequest();
+		xhr.open(form.method, form.action);
+		xhr.setRequestHeader("Accept", "application/json");
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState !== XMLHttpRequest.DONE) return;
+			if (xhr.status === 200) {
+				form.reset();
+				this.setState({ status: "SUCCESS" });
+			} else {
+				this.setState({ status: "ERROR" });
+			}
+		};
+		xhr.send(data);
+	}
+}
